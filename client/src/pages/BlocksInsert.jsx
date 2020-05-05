5f88b7568c84b6b608afadb60a3486c2ba3e8189
littleashsapling <59930398+littleashsapling@users.noreply.github.com>
2020-05-05 19:20:55 -0300

    revenge of the 5th

--- client/src/pages/BlocksInsert.jsx
+++ client/src/pages/BlocksInsert.jsx
@@ -1,6 +1,32 @@
 import React, { Component } from 'react'
+import {beginAddImage} from '../actions/images'
+import api from '../api'
+
+
 
 class BlocksInsert extends Component{
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            imageData: '',
+            name: '',
+            nameSpace: '',
+            id:'',
+            tags:'',
+            color:'',
+            blastResistance:'',
+            luminant:'',
+            flammable:'',
+            gravity:'',
+            location:'',
+            versionPC:'',
+            bedrock:'',
+            versionConsole:'',
+            
+        }
+    }
     render() {
         return(
             <div>
